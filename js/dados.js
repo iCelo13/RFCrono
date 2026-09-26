292656987df5a302facc2fbd3c399be806a70d1d
iCelo13 <96854164+iCelo13@users.noreply.github.com>
2026-09-26 13:58:10 +0100

    Novo cartaz de entrada

--- js/dados.js
+++ js/dados.js
@@ -5,7 +5,7 @@
 
 const DADOS = {
   "popup": {
-    "imagem": "img/cartazes/2026-09-20-28240.jpg",
+    "imagem": "img/cartazes/2026-09-26-30790.jpg",
     "alt": "Cartaz do próximo evento"
   },
   "proximosEventos": [
